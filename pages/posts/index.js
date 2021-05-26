f5d299d86dfab0f4196b15c36860bb7bd30d67f1
Mikolertesx <mgangsal@outlook.com>
2021-05-26 11:40:20 -0500

    Added an all-posts page.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,25 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "nextjs-file-based-routing",
+    title: "NextJS file based routing",
+    image: "nextjs-file-based-routing.png",
+    excerpt: "NextJS uses a very dynamic way of rendering different pages.",
+    date: "2022-03-11",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
