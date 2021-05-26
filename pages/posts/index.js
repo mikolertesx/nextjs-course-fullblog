ee6f1e675711d5f2b790c396acb5a7d99e7eb942
Mikolertesx <mgangsal@outlook.com>
2021-05-26 13:53:29 -0500

    Changed the posts origin.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,19 +1,8 @@
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with NextJs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10",
-  },
-];
-
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts} />;
 }
 
 export function getStaticProps() {
