dde2147a99cd5522862669d2ddf23100b58046da
Mikolertesx <mgangsal@outlook.com>
2021-05-26 14:40:16 -0500

    Added code syntax support

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
