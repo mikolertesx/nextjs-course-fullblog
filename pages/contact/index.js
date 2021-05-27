2b0079c5da8978470e45d556b274cfc85fe810bd
Mikolertesx <mgangsal@outlook.com>
2021-05-27 13:21:07 -0500

    Added a contact form.

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,3 +1,7 @@
-function ContactPage() {}
+import ContactForm from "../components/contact/contact-form";
+
+const ContactPage = (props) => {
+  return <ContactForm />;
+};
 
 export default ContactPage;
