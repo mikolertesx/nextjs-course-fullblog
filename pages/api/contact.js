791d147b0906f90591c495e63f017e893edc9047
Miguel Guerrero <mgangsal@outlook.com>
2021-06-26 09:54:44 -0500

    Added mongo client for Form request

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
 	if (req.method === "POST") {
 		const { email, name, message } = req.body;
 		if (
@@ -19,10 +21,32 @@ function handler(req, res) {
 			name,
 			message,
 		};
+		let client;
 
-		console.log(newMessage);
+		try {
+			client = await MongoClient.connect(
+				"mongodb+srv://mikolertesx:guapon@cluster0.uo3pj.mongodb.net/my-site?retryWrites=true&w=majority",
+				{ useNewUrlParser: true }
+			);
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+			return;
+		}
 
-		res
+		const db = client.db();
+		let result;
+
+		try {
+			result = await db.collection("messages").insertOne(newMessage);
+			newMessage.id = result.insertedId;
+		} catch (error) {
+			client.close();
+			return res.status(500).json({ message: "Storing message failed!" });
+		}
+
+		client.close();
+
+		return res
 			.status(201)
 			.json({ message: "Successfully stored message!", message: newMessage });
 	}
